ecfd2b9cd07456d11b9e0715c6ecb0b2569e6ef1
deveshjoshi101 <deveshjoshi101@gmail.com>
2024-06-02 20:51:02 +0530

    navbar

---
+++ navbar/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./components/Button";
+import Navbar from "./components/Navbar/Navbar";
+
+const App = () => {
+  return (
+    <section>
+      <Navbar />
+    </section>
+  );
+};
+
+export default App;
